e9edd0bfbda8b7ae151d3ab603bf6f343a3eb982
Ayrton <ayrton.guerra@cliqx.com.br>
2023-04-01 16:10:55 -0300

    chore: add prettier lib

--- src/instances/pg.ts
+++ src/instances/pg.ts
@@ -1,7 +1,7 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
+import { Sequelize } from "sequelize"
+import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config()
 
 export const sequelize = new Sequelize(
   process.env.PG_DB as string,
@@ -11,4 +11,4 @@ export const sequelize = new Sequelize(
     dialect: "postgres",
     port: parseInt(process.env.PG_PORT as string),
   }
-);
+)
